868ed1c1c00b1aa4328b7c5adaab01de17622faa
Brandon Dybala <dybalabj@gmail.com>
2013-04-05 23:19:11 -0500

    Initial project setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Budget::Application.config.secret_token = 'a96fd17184cad37d187a6817fee4d57c493d0693f9fc941ef73e944f6bd31b9f3897ad35bbea422da3719e0c5a32cd0ed3c1acc41315387a5db31994c196a25e'
