8c95a104231af61d3dc087f19ae8dd38b5c89bcb
Brandon Dybala <dybalabj@gmail.com>
2013-04-06 00:23:25 -0500

    Initialize default values for models.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,4 +2,10 @@ class Payment < ActiveRecord::Base
   belongs_to :paymentMethod
   has_many :paymentCategories, :through => :paymentCategoryLink
   attr_accessible :amount, :description, :modified, :purchaseDate
+  
+  after_initialize :defaults
+  
+  def defaults
+    self.purchaseDate = Time.now if self.purchaseDate.nil?
+  end
 end
--- app/models/payment_category.rb
+++ app/models/payment_category.rb
@@ -1,4 +1,12 @@
 class PaymentCategory < ActiveRecord::Base
   has_many :payments, :through => :paymentCategoryLink
   attr_accessible :active, :name, :sortOrder
+  
+  after_initialize :defaults
+  
+  def defaults
+    self.active = true if self.active.nil?
+    # default sort order to the last category.
+    self.sortOrder = PaymentCategory.count if self.sortOrder.nil?
+  end
 end
