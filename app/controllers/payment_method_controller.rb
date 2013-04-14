45401ad081e82850cc04b64c0c69502132fefe79
Brandon Dybala <dybalabj@gmail.com>
2013-04-13 23:39:49 -0500

    Implemented view and controller actions for displaying and editing payment methods.

--- app/controllers/payment_method_controller.rb
+++ app/controllers/payment_method_controller.rb
@@ -4,8 +4,17 @@ class PaymentMethodController < ApplicationController
   end
 
   def new
+    # Render default new template.
   end
 
   def edit
+    @paymentMethod = PaymentMethod.find params[:id]
+  end
+  
+  def update
+    @paymentMethod = PaymentMethod.find params[:id]
+    @paymentMethod.update_attributes!(params[:paymentMethod])
+    flash[:notice] = "'#{@paymentMethod.name}' has been updated"
+    redirect_to payment_method_index_path
   end
 end
